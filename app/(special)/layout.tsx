744c20a4e9a7c902a6d3607ff33d15ab9eef00a8
Scorpion647 <jhoyflow15@gmail.com>
2024-08-24 23:52:54 -0500

    Cambios usuario y admin

--- app/(special)/layout.tsx
+++ app/(special)/layout.tsx
@@ -1,31 +1,45 @@
-"use client";
-
-import { checkIfAdmin } from '@/app/_lib/supabase/client';
-import { useEffect, useState } from 'react';
+"use server";
+import { getRole } from '@/app/_lib/supabase/server';
+import { Suspense } from 'react';
 import { CustomDataError } from '../_lib/definitions';
 import PageLoader from '../_ui/pageLoader';
 
-export default function SpecialLayout({ user, admin }: {
+export default async function SpecialLayout({ user, admin, guest }: {
     user: React.ReactNode,
     admin: React.ReactNode,
+    guest: React.ReactNode,
 }) {
-    const [isAdmin, setIsAdmin] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+    let userRole: string = "guest";
+    let isLoading = true;
 
-    useEffect(() => {
-        checkIfAdmin().then(isAdmin => {
-            if (isAdmin instanceof CustomDataError) {
-                // TODO: handle error
-            } else {
-                setIsAdmin(isAdmin);
-            }
-            setIsLoading(false);
-        });
-    }, [])
+    getRole().then(role => {
+        if (role instanceof CustomDataError) {
+            // TODO: handle error
+        } else if (role) {
+            userRole = role;
+        } else {
+            // TODO: handle unexpectede rror
+        }
+        isLoading = false;
+    });
 
     return (
         <main className='flex flex-col items-center justify-center h-screen'>
-            {isLoading ? <PageLoader /> : isAdmin ? admin : user}
+            <Suspense fallback={<PageLoader />}>
+                {getRole().then(role => {
+                    if (role instanceof CustomDataError) {
+                        // TODO: handle error
+                    } else if (role) {
+                        userRole = role;
+                    } else {
+                        // TODO: handle unexpected error
+                    }
+                    isLoading = false;
+                    return <>
+                        {isLoading ? <PageLoader /> : userRole === "administrator" ? admin : userRole === "employee" ? user : guest}
+                    </>;
+                })}
+            </Suspense>
         </main>
     )
 }
