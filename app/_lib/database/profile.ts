271bb27973376e7895d85164424178c64dcbd731
pablohernandezm <phernandezm07@gmail.com>
2024-09-03 08:18:35 -0500

    Delete account functions

--- app/_lib/database/profile.ts
+++ app/_lib/database/profile.ts
@@ -34,3 +34,41 @@ export async function updateProfile(data: TablesUpdate<'profile'>): Promise<void
         if (updateError) return manageErrorMessage(updateError);
     }
 }
+
+export async function deleteAccount(): Promise<void | CustomDataError> {
+    const supabase = createClient()
+
+    const { data: userData, error: userError } = await supabase.auth.getUser()
+
+    if (userError) return manageErrorMessage(userError);
+
+    if (userData.user) {
+        const { error } = await supabase.functions.invoke('delete-user', { body: { user_id: userData.user.id } })
+        if (error) return manageErrorMessage(error);
+    } else {
+        return manageErrorMessage(null)
+    }
+}
+
+export async function removeUser(user_id: string): Promise<void | CustomDataError> {
+    const supabase = createClient()
+
+    const { data: userData, error: userError } = await supabase.auth.getUser()
+
+    if (userError) return manageErrorMessage(userError);
+
+    if (userData.user) {
+        const { data: userTarget, error: userTargetError } = await supabase.from('profile').select('user_id').eq('user_id', user_id).limit(1).maybeSingle()
+
+        if (userTargetError) return manageErrorMessage(userTargetError);
+        if (!userTarget) return manageErrorMessage(null)
+
+        if (userTarget?.user_id) {
+            const { error } = await supabase.functions.invoke('delete-user', { body: { user_id: userTarget.user_id } })
+            if (error) return manageErrorMessage(error);
+        }
+
+    } else {
+        return manageErrorMessage(null)
+    }
+}
---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,4 @@
+export const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Heders': 'authorization, x-client-info, api-key, content-type',
+}
