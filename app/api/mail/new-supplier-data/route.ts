fc0a21c0f7b3049b0716db299813cb7fd58d9529
Pablo José Hernández Meléndez <phernandezm07@gmail.com>
2024-09-25 17:02:12 -0500

    fix: email route request

--- app/api/mail/new-supplier-data/route.ts
+++ app/api/mail/new-supplier-data/route.ts
@@ -1,20 +1,35 @@
 import { EmailTemplate } from "@email-templates/supplier-data";
+import { NextApiRequest, NextApiResponse } from "next";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { email, purchase_order, bill, date, supplier_name, invoice_id } =
+    await req.body;
+
+  if (
+    !email ||
+    !purchase_order ||
+    !bill ||
+    !date ||
+    !supplier_name ||
+    !invoice_id
+  ) {
+    return res.status(400).json({ error: { message: "Invalid request" } });
+  }
+
   try {
     const { data, error } = await resend.emails.send({
       from: "Zofzf team <team@zofzf.online>",
-      to: ["phernandezm07@gmail.com"],
-      subject: "Hello Pablo",
+      to: [email],
+      subject: "Solicitud recibida",
       react: EmailTemplate({
-        purchase_order: "PO01",
-        bill: "DDQ23",
-        date: new Date().toLocaleDateString(),
-        supplier: "Testing@1",
-        invoice_id: "AD213ADS",
+        purchase_order: purchase_order,
+        bill: bill,
+        date: date,
+        supplier: supplier_name,
+        invoice_id: invoice_id,
       }),
     });
 
