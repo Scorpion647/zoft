3f6d8a2b675f8a92cdfce73ddb10dceb4c7781a5
Pablo José Hernández Meléndez <phernandezm07@gmail.com>
2024-09-30 11:57:30 -0500

    fix: bill quantities on delete and update

--- supabase/migrations/20240930001436_bill-quantities.sql
+++ supabase/migrations/20240930001436_bill-quantities.sql
@@ -7,13 +7,13 @@ begin
 
     if (invoice.state = 'approved') then
         update public.base_bills
-        set approved_quantity=approved_quantity+new.billed_quantity
+        set approved_quantity=approved_quantity+new.billed_quantity-coalesce(old.billed_quantity, 0)
         where base_bills.base_bill_id = new.base_bill_id;
 
         update public.invoice_data set state = 'pending' where invoice_id = new.invoice_id;
     elseif (invoice.state = 'pending') then
         update public.base_bills
-        set pending_quantity=pending_quantity+new.billed_quantity
+        set pending_quantity=pending_quantity + new.billed_quantity - coalesce(old.billed_quantity, 0)
         where base_bills.base_bill_id = new.base_bill_id;
 
     else
@@ -29,24 +29,15 @@ end;
 $$ language plpgsql security definer;
 
 
-CREATE TRIGGER update_base_bill_quantity_after_insert
+CREATE TRIGGER update_base_bill_quantity_after_supplier_data_insert
 AFTER insert ON public.supplier_data FOR each ROW
 EXECUTE procedure public.update_invoice_state ();
 
 
-CREATE FUNCTION public.update_invoice_state_after_update () returns trigger AS $$
-begin
-    update public.invoice_data set state = state where invoice_id = new.invoice_id;
-
-    return new;
-end;
-$$ language plpgsql security definer;
-
-
-CREATE TRIGGER update_base_bill_quantity_after_update
+CREATE TRIGGER update_base_bill_quantity_after_supplier_data_update
 AFTER
 UPDATE ON public.supplier_data FOR each ROW
-EXECUTE procedure public.update_invoice_state_after_update ();
+EXECUTE procedure public.update_invoice_state ();
 
 
 CREATE
@@ -89,6 +80,10 @@ begin
                 set approved_quantity=approved_quantity+_quantity
                 where base_bills.base_bill_id = _bill_record.base_bill_id;
             else
+                if (new.state = 'rejected') then
+                    update public.invoice_data set state = 'pending' where invoice_id = new.invoice_id;
+                end if;
+
                 update public.base_bills
                 set pending_quantity=pending_quantity+_quantity
                 where base_bills.base_bill_id = _bill_record.base_bill_id;
@@ -105,3 +100,51 @@ CREATE TRIGGER handle_invoice_state_change
 AFTER
 UPDATE ON public.invoice_data FOR each ROW
 EXECUTE procedure public.handle_invoice_state_change ();
+
+
+CREATE
+OR REPLACE function public.update_invoice_state_after_supplier_data_delete () returns trigger AS $$
+    declare
+        invoice invoice_data%rowtype;
+begin
+    select * into invoice from public.invoice_data where invoice_id = old.invoice_id;
+
+    if (invoice.state = 'approved') then
+        update public.base_bills
+        set approved_quantity=approved_quantity-old.billed_quantity
+        where base_bills.base_bill_id = old.base_bill_id;
+
+        update public.invoice_data set state = 'pending' where invoice_id = old.invoice_id;
+    elseif (invoice.state = 'pending') then
+        update public.base_bills
+        set pending_quantity=pending_quantity-old.billed_quantity
+        where base_bills.base_bill_id = old.base_bill_id;
+        raise warning 'pending %', old.billed_quantity;
+
+    else
+        update public.invoice_data set state = 'pending' where invoice_id = old.invoice_id;
+    end if;
+
+    return old;
+end;
+$$ language plpgsql security definer;
+
+
+CREATE TRIGGER update_base_bill_quantity_after_supplier_data_delete
+AFTER delete ON public.supplier_data FOR each ROW
+EXECUTE procedure public.update_invoice_state_after_supplier_data_delete ();
+
+
+-- on invoice delete, delete all supplier data. It is necessary to do this because of the trigger on supplier_data
+CREATE
+OR REPLACE function public.delete_invoice_data () returns trigger AS $$
+begin
+    delete from public.supplier_data where invoice_id = old.invoice_id;
+
+    return old;
+end;
+$$ language plpgsql security definer;
+
+
+CREATE TRIGGER delete_invoice_data before delete ON public.invoice_data FOR each ROW
+EXECUTE procedure public.delete_invoice_data ();
