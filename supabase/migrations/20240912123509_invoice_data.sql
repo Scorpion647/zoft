18f643054a3064e03b624bcdfa136df05f096225
Pablo José Hernández Meléndez <phernandezm07@gmail.com>
2024-10-11 14:43:22 -0500

    feat: feedback text

--- supabase/migrations/20240912123509_invoice_data.sql
+++ supabase/migrations/20240912123509_invoice_data.sql
@@ -8,6 +8,7 @@ CREATE TABLE public.invoice_data (
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
   state public.invoice_state DEFAULT 'pending' NOT NULL,
   last_modified_by UUID DEFAULT auth.uid (),
+  feedback TEXT,
   PRIMARY KEY (invoice_id),
   FOREIGN key (supplier_id) REFERENCES public.suppliers (supplier_id) ON DELETE cascade ON UPDATE cascade,
   FOREIGN key (last_modified_by) REFERENCES public.profiles (profile_id) ON DELETE SET NULL ON UPDATE cascade
